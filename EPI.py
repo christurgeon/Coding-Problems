407ef8872403cc8c52017f1d7bc1030e87f129fe
Chris Turgeon <46657395+christurgeon@users.noreply.github.com>
2021-07-23 17:19:06 -0400

    Update EPI.py

--- EPI.py
+++ EPI.py
@@ -747,18 +747,11 @@ def cyclicallyRightShiftList(L: ListNode, k: int):
 
 
 def removeDuplicates(L: ListNode):
-    pass 
-
-
-####################################################################################################
-#                                           STRINGS                                                #
-####################################################################################################
-
-####################################################################################################
-#                                           SEARCH                                                 #
-####################################################################################################
-
-
-
-
-
+    it = L
+    while it:
+        next_distinct = it.next
+        while next_distinct and next_distinct.data == it.data:
+            next_distinct = next_distinct.next
+        it.next = next_distinct
+        it = next_distinct
+    return L
