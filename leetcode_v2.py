8ee97bbe845f9350ec2b03039e34b3390ee4f047
Chris <46657395+christurgeon@users.noreply.github.com>
2024-08-07 18:45:15 -0400

    Create leetcode_v2.py

---
+++ leetcode_v2.py
@@ -0,0 +1,33 @@
+##################################################
+# A collection of additional LeetCode problems 
+# since leetcode.py is getting a bit too large...
+##################################################
+
+
+# https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+def countPairs(nums: List[int], target: int) -> int:
+    size = len(nums)
+    result = 0
+    for idx_i, i in enumerate(nums):
+        for idx_j in range(idx_i + 1, size):
+            if i + nums[idx_j] < target:
+                result += 1
+    return result
+
+
+# https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/ 
+def countNegatives(self, grid: List[List[int]]) -> int:
+    depth = len(grid)
+    width = len(grid[0])
+    r_ptr = 0
+    c_ptr = width - 1
+    result = 0
+
+    while r_ptr < depth and c_ptr >= 0:
+        if grid[r_ptr][c_ptr] < 0:
+            result += depth - r_ptr
+            c_ptr -= 1
+        else:
+            r_ptr += 1
+          
+    return result
