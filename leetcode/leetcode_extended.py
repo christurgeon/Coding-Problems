d684d98270d26bad8b30e79fa7aebbd1c7570e10
Chris <46657395+christurgeon@users.noreply.github.com>
2025-08-29 22:59:15 -0400

    Update leetcode_extended.py

--- leetcode/leetcode_extended.py
+++ leetcode/leetcode_extended.py
@@ -2604,3 +2604,56 @@ def containsNearbyAlmostDuplicate(nums: List[int], indexDiff: int, valueDiff: in
             oldBucketId = valueToRemove // bucketSize
             del bucket[oldBucketId]
     return False
+
+
+# https://leetcode.com/problems/integer-to-english-words/
+def numberToWords(num: int) -> str:
+    lookup_digit = {
+        "0": "Zero", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"
+    }
+    lookup_teens = {
+        "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen", "15": "Fifteen", "16": "Sixteen", "17": "Seventeen", "18": "Eighteen", "19": "Nineteen"
+    }
+    lookup_tens = {
+        "2": "Twenty", "3": "Thirty", "4": "Forty", "5": "Fifty", "6": "Sixty", "7": "Seventy", "8": "Eighty", "9": "Ninety"
+    }
+    lookup_larger = ["", "Thousand", "Million", "Billion", "Trillion"]
+
+    if num < 10:
+        return lookup_digit[str(num)]
+
+    def processThreeDigitChunk(chunk):
+        chunk_result = []
+        if len(chunk) == 3 and chunk[0] != "0":
+            chunk_result.append(lookup_digit[chunk[0]])
+            chunk_result.append("Hundred")
+        if len(chunk) >= 2 and chunk[-2] != "0":
+            if chunk[-2:] == "10":
+                chunk_result.append("Ten")
+                return chunk_result
+            if chunk[-2] == "1":
+                chunk_result.append(lookup_teens[chunk[-2:]])
+                return chunk_result
+            chunk_result.append(lookup_tens[chunk[-2]])
+        if len(chunk) >= 1 and chunk[-1] != "0":
+            chunk_result.append(lookup_digit[chunk[-1]])
+        return chunk_result
+
+    result = deque([])
+    value = str(num)
+    end = len(value) - 1
+    placeholder = 0
+    while end >= 0:
+        chunk = value[max(0,end-2):end+1]
+        chunk_result = processThreeDigitChunk(chunk)
+        # Determine if we should add a "thousand", "million", etc.
+        if chunk_result:
+            if placeholder > 0:
+                result.appendleft(lookup_larger[placeholder])
+            # Add our next chunk to the result
+            for s in reversed(chunk_result):
+                result.appendleft(s)
+        end -= 3
+        placeholder += 1
+
+    return " ".join(result)
